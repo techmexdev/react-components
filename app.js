546bfc3676549ad35aff55265b57633af2828fb1
Fliko <baxter.flw@gmail.com>
2017-04-07 11:53:05 -0500

    Create a Grocery List

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    }
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)} >{this.props.food}</li>
+    );
+
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.food.map((thing) => <GroceryListItem food={thing}/>)}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList food={['Peanut Butter', 'Bananas', 'Meat']} /> 
+  </div>
+);
+
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
